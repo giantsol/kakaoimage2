b2f8ae3c9b4793003484681c6c3925721bab88e0
younditt <dhy039@gmail.com>
2020-07-26 20:43:50 +0900

    빌드그래들&readme 정리

--- app/src/main/java/com/ellen/kakaoimages/di/ViewModelModule.kt
+++ app/src/main/java/com/ellen/kakaoimages/di/ViewModelModule.kt
@@ -2,7 +2,6 @@ package com.ellen.kakaoimages.di
 
 import com.ellen.kakaoimages.viewmodel.ImageViewModel
 import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.androidx.fragment.dsl.fragment
 import org.koin.dsl.module
 
 val viewModelModule = module {
